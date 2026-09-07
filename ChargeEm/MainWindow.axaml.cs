34ef492109ba8f3eb62a5d307dab1533d245d942
Tyler Millershaski <millershaski@gmail.com>
2026-09-07 13:29:46 -0400

    Ported the main window code

--- ChargeEm/MainWindow.axaml.cs
+++ ChargeEm/MainWindow.axaml.cs
@@ -1,11 +1,24 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
-namespace ChargeEm;
+namespace ChargeEmQuoteUI;
 
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void GenerateQuoteButton_Click(object? sender, RoutedEventArgs e)
+    {
+    }
+
+    private void ClearButton_Click(object? sender, RoutedEventArgs e)
+    {
+    }
+
+    private void ExitButton_Click(object? sender, RoutedEventArgs e)
+    {
     }
 }
