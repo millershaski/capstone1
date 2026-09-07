368ed4c3d9119e70bd28067f44c2277229d08d99
Tyler Millershaski <millershaski@gmail.com>
2026-09-07 16:43:17 -0400

    Initial port to visual studio

---
+++ ChargeEm/ChargeEm/MainForm.cs
@@ -0,0 +1,27 @@
+namespace ChargeEm
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        void GenerateQuoteButton_Click(object? sender, EventArgs e)
+        {
+        }
+
+
+
+        void ClearButton_Click(object? sender, EventArgs e)
+        {
+        }
+
+
+
+        void ExitButton_Click(object? sender, EventArgs e)
+        {
+        }
+    }
+}
